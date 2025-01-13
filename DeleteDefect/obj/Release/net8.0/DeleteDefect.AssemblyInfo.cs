8948cdef65b3534d245908a3206bf7a3b0bf9891
ppattxx <acahyadava@gmail.com>
2025-01-13 15:18:18 +0700

    remove id csv

--- DeleteDefect/obj/Release/net8.0/DeleteDefect.AssemblyInfo.cs
+++ DeleteDefect/obj/Release/net8.0/DeleteDefect.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeleteDefect")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea23134b74476840d3b55c2c3b7ac12638df29a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeleteDefect")]
